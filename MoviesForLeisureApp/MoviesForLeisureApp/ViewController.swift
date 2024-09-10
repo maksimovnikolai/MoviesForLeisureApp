02cec569b932c87d2079f755d74a1245bd37de9f
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-09-10 21:33:54 +0300

    добавлен дефолтный проект

---
+++ MoviesForLeisureApp/MoviesForLeisureApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesForLeisureApp
+//
+//  Created by Nikolai Maksimov on 10.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+
+
+}
+
