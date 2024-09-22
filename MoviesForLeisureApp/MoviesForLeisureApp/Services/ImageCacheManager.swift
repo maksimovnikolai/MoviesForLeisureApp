4a92f71344cc30ecddf380ec4a2c2e3f4f8c69d5
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-09-22 22:14:11 +0300

    добавил методы сохранения и загрузки изображения в/из  кеш

--- MoviesForLeisureApp/MoviesForLeisureApp/Services/ImageCacheManager.swift
+++ MoviesForLeisureApp/MoviesForLeisureApp/Services/ImageCacheManager.swift
@@ -7,7 +7,21 @@
 
 import UIKit
 
-final class ImageCacheManager {
-    static let shared = NSCache<NSString, UIImage>()
-    private init() {}
+protocol ImageCacheManagerProtocol {
+    func saveToCache(imageData: Data, forKey url: URL)
+    func loadImageDataFromCache(from url: URL) -> Data?
+}
+
+final class ImageCacheManager1: ImageCacheManagerProtocol {
+    private let cache = NSCache<NSString, UIImage>()
+    
+    func saveToCache(imageData: Data, forKey url: URL) {
+        guard let image = UIImage(data: imageData) else { return }
+        cache.setObject(image, forKey: url.deletingLastPathComponent().lastPathComponent as NSString)
+    }
+    
+    func loadImageDataFromCache(from url: URL) -> Data? {
+       let image = cache.object(forKey: url.deletingLastPathComponent().lastPathComponent as NSString)
+        return image?.pngData()
+    }
 }
