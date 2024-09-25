2e62811a969622b4f04e4b06767f97c463adb002
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-09-25 20:39:25 +0300

    добавил протокол с методами (commonInit, setupSubviews, setupConstraints)

---
+++ MoviesForLeisureApp/MoviesForLeisureApp/Protocols/CommonInitProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  CommonInitProtocol.swift
+//  MoviesForLeisureApp
+//
+//  Created by Nikolai Maksimov on 25.09.2024.
+//
+
+import Foundation
+
+protocol CommonInitProtocol {
+    func commonInit()
+    func setupSubviews()
+    func setupConstraints()
+}
