462d95c6ac6aad2bca7e1adb893407176dd57bb2
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-09-25 20:56:47 +0300

    объединил  кастомные view в одно + scrollView

--- MoviesForLeisureApp/MoviesForLeisureApp/MovieDetails/View/MovieDetailsView.swift
+++ MoviesForLeisureApp/MoviesForLeisureApp/MovieDetails/View/MovieDetailsView.swift
@@ -9,8 +9,25 @@ import UIKit
 import SnapKit
 
 final class MovieDetailsView: UIView {
+    // MARK: - UI components
+    
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        return scrollView
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.spacing = 10
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    private let posterView = MoviePosterView()
+    private var descriptionView = MovieDescriptionView()
 
     // MARK: - Init
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
@@ -21,19 +38,53 @@ final class MovieDetailsView: UIView {
     }
 }
 
-// MARK: - Private methods
+// MARK: - ViewModelConfigurable
 
-private extension MovieDetailsView {
+extension MovieDetailsView {
+    func configure(with model: Model) {
+        descriptionView.configure(title: model.movieTitle, description: model.movieDescription)
+    }
+}
+
+// MARK: - Model
+
+extension MovieDetailsView {
+    struct Model {
+        var movieTitle: String
+        var movieDescription: String
+    }
+}
+
+// MARK: - CommonInitProtocol
+
+extension MovieDetailsView: CommonInitProtocol {
     func commonInit() {
+        backgroundColor = .systemBackground
         setupSubviews()
         setupConstraints()
     }
     
     func setupSubviews() {
-
+        [posterView, descriptionView].forEach { stackView.addArrangedSubview($0) }
+        addSubview(scrollView)
+        scrollView.addSubview(stackView)
     }
     
     func setupConstraints() {
-     
+        scrollView.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.bottom.equalTo(safeAreaLayoutGuide)
+            make.width.equalTo(stackView.snp.width)
+        }
+        
+        stackView.snp.makeConstraints { make in
+            make.horizontalEdges.verticalEdges.equalTo(scrollView)
+        }
+        
+        posterView.snp.makeConstraints { make in
+            make.height.equalTo(250)
+        }
     }
 }
