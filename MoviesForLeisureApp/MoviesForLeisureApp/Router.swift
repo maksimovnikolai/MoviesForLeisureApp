efd9a33672b7bf4a5862fc59a0e72451e528019e
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-10-13 19:28:02 +0300

    переписал новый router

--- MoviesForLeisureApp/MoviesForLeisureApp/Router.swift
+++ MoviesForLeisureApp/MoviesForLeisureApp/Router.swift
@@ -7,25 +7,126 @@
 
 import UIKit
 
-protocol RouterHelperProtocol {
-    var navigationController: UINavigationController? { get set }
+protocol Presentable {
+    func toPresent() -> UIViewController?
 }
 
-protocol RouterProtocol: RouterHelperProtocol {
-    func showMovieDetails(_ item: Movie)
+extension UIViewController: Presentable {
+    func toPresent() -> UIViewController? {
+        return self
+    }
+}
+
+protocol RouterProtocol {
+    func present(_ module: Presentable?)
+    func present(_ module: Presentable?, animated: Bool)
+    
+    func push(_ module: Presentable?)
+    func push(_ module: Presentable?, hideBottomBar: Bool)
+    func push(_ module: Presentable?, animated: Bool)
+    func push(_ module: Presentable?, animated: Bool, completion: (() -> Void)?)
+    func push(_ module: Presentable?, animated: Bool, hideBottomBar: Bool, completion: (() -> Void)?)
+    
+    func popModule()
+    func popModule(animated: Bool)
+    
+    func dismissModule()
+    func dismissModule(animated: Bool, completion: (() -> Void)?)
+    
+    func setRootModule(_ module: Presentable?)
+    func setRootModule(_ module: Presentable?, hideBar: Bool)
+    
+    func popToRootModule(animated: Bool)
 }
 
 final class Router: RouterProtocol {
-    var navigationController: UINavigationController?
+    private weak var rootController: UINavigationController?
+    private var completions: [UIViewController: () -> Void]
     
-    init(navigationController: UINavigationController) {
-        self.navigationController = navigationController
+    init(rootController: UINavigationController) {
+        self.rootController = rootController
+        completions = [:]
     }
     
-    func showMovieDetails(_ item: Movie) {
-        if let navigationController {
-            let controller = MovieDetailsFactory().createMovieDetails(item)
-            navigationController.pushViewController(controller, animated: true)
+    func toPresent() -> UIViewController? {
+        rootController
+    }
+    
+    func present(_ module: (any Presentable)?) {
+        present(module, animated: true)
+    }
+    
+    func present(_ module: (any Presentable)?, animated: Bool) {
+        guard let controller = module?.toPresent() else { return }
+        rootController?.present(controller, animated: animated)
+    }
+    
+    func push(_ module: (any Presentable)?) {
+        push(module, animated: true)
+    }
+    
+    func push(_ module: (any Presentable)?, hideBottomBar: Bool) {
+        push(module, animated: true, hideBottomBar: hideBottomBar, completion: nil)
+    }
+    
+    func push(_ module: (any Presentable)?, animated: Bool) {
+        push(module, animated: animated, completion: nil)
+    }
+    
+    func push(_ module: (any Presentable)?, animated: Bool, completion: (() -> Void)?) {
+        push(module, animated: animated, hideBottomBar: false, completion: completion)
+    }
+    
+    func push(_ module: (any Presentable)?, animated: Bool, hideBottomBar: Bool, completion: (() -> Void)?) {
+        guard let controller = module?.toPresent(), (controller is UINavigationController == false) else { assertionFailure("Deprecated push UINavigationController."); return }
+        
+        if let completion = completion {
+            completions[controller] = completion
         }
+        
+        controller.hidesBottomBarWhenPushed = hideBottomBar
+        rootController?.pushViewController(controller, animated: animated)
+    }
+    
+    func popModule() {
+        popModule(animated: true)
+    }
+    
+    func popModule(animated: Bool) {
+        if let controller = rootController?.popViewController(animated: animated){
+            runCompletion(for: controller)
+        }
+    }
+    
+    func dismissModule() {
+        dismissModule(animated: true, completion: nil)
+    }
+    
+    func dismissModule(animated: Bool, completion: (() -> Void)?) {
+        rootController?.dismiss(animated: animated, completion: completion)
+    }
+    
+    func setRootModule(_ module: (any Presentable)?) {
+        setRootModule(module, hideBar: false)
+    }
+    
+    func setRootModule(_ module: (any Presentable)?, hideBar: Bool) {
+        guard let controller = module?.toPresent() else { return }
+        rootController?.setViewControllers([controller], animated: false)
+        rootController?.isNavigationBarHidden = hideBar
+    }
+    
+    func popToRootModule(animated: Bool) {
+        if let controllers = rootController?.popToRootViewController(animated: animated) {
+            controllers.forEach { controller in
+                runCompletion(for: controller)
+            }
+        }
+    }
+    
+    private func runCompletion(for controller: UIViewController) {
+        guard let completion = completions[controller] else { return }
+        completion()
+        completions.removeValue(forKey: controller)
     }
 }
