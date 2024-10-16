c82f2a7fb69e8e658b73c34aeff6263c8a71ca39
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-10-16 22:37:09 +0300

    viewmodel для loginView

--- MoviesForLeisureApp/MoviesForLeisureApp/Login/LoginViewController.swift
+++ MoviesForLeisureApp/MoviesForLeisureApp/Login/LoginViewController.swift
@@ -11,7 +11,19 @@ final class LoginViewController: UIViewController {
 
     // MARK: - Private properties
     
-    private var loginView = LoginView()
+    private let viewModel: LoginViewModelProtocol
+    private let loginView = LoginView()
+    
+    // MARK: - Init
+    
+    init(viewModel: LoginViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARK: - Life cycle
     
---
+++ MoviesForLeisureApp/MoviesForLeisureApp/Login/LoginViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewModel.swift
+//  MoviesForLeisureApp
+//
+//  Created by Nikolai Maksimov on 16.10.2024.
+//
+
+import Foundation
+
+protocol LoginViewModelProtocol {}
+
+final class LoginViewModel {
+    // TODO: - Добавить логику входа в аккаунт
+}
+
+// MARK: - LoginViewModelProtocol
+extension LoginViewModel: LoginViewModelProtocol {}
