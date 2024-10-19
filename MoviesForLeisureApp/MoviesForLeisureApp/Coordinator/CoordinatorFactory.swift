ca6ff45b453b1664940a5eb7e90244012e58c16d
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-10-19 23:59:18 +0300

    добавил методы создания координаторов в фабрике

--- MoviesForLeisureApp/MoviesForLeisureApp/Coordinator/CoordinatorFactory.swift
+++ MoviesForLeisureApp/MoviesForLeisureApp/Coordinator/CoordinatorFactory.swift
@@ -9,10 +9,20 @@ import Foundation
 
 protocol CoordinatorFactoryProtocol {
     func makeApplicationCoordinator(router: RouterProtocol) -> ApplicationCoordinator
+    func makeLoginCoordinator(router: RouterProtocol) -> LoginCoordinator
+    func makeTabBarCoordinator(router: RouterProtocol) -> TabBarCoordinator
 }
 
 final class CoordinatorFactory: CoordinatorFactoryProtocol {
     func makeApplicationCoordinator(router: RouterProtocol) -> ApplicationCoordinator {
         ApplicationCoordinator(router: router, coordinatorFactory: self)
+    }
+    
+    func makeLoginCoordinator(router: RouterProtocol) -> LoginCoordinator {
+        LoginCoordinator(router: router)
+    }
+    
+    func makeTabBarCoordinator(router: RouterProtocol) -> TabBarCoordinator {
+        TabBarCoordinator(router: router)
     }
 }
